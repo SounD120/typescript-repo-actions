f2e494ff30a739bcd0d3afa6abe828447fdd8c6f
SounD <yurapkr@yandex.ru>
2023-05-01 14:32:38 +0300

    Add GH action

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,72 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const mode = core.getInput('action-mode')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const owner = core.getInput('owner')
+    const repo = core.getInput('repo')
 
-    core.setOutput('time', new Date().toTimeString())
+    let query: { state: string, since?: string } = { state: 'all' }
+
+    switch (mode) {
+      case 'issues-all': {
+        break
+      }
+      case 'issues-open': {
+        query = { state: 'open' }
+        break
+      }
+      case 'issues-closed': {
+        query = { state: 'closed' }
+        break
+      }
+      case 'issues-opened-since-date': {
+        const date = core.getInput('issues-from-date')
+
+        let dateParsed
+        if (date) {
+          dateParsed = Date.parse(date)
+          if (!dateParsed) {
+            throw new Error('Incorrect Date')
+          }
+
+          // Convert timestamp to ISO8601 string
+          dateParsed = new Date(dateParsed).toISOString()
+        } else {
+          const dateNow = new Date()
+
+          // Get the first day of the previous month and convert to ISO8601
+          dateParsed = new Date(dateNow.getFullYear(), dateNow.getMonth() - 1, 1).toISOString()
+        }
+
+        query = { state: 'all', since: dateParsed }
+        break
+      }
+      default: {
+        throw new Error('action-mode value is not issues-all, issues-open, issues-closed or issues-opened-since-date')
+      }
+    }
+
+    const octokit = github.getOctokit(process.env.TOKEN || '')
+    const result = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+      owner,
+      repo,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+        accept: 'application/vnd.github+json',
+      },
+      query
+    })
+
+    core.setOutput('response', result.data.length || 0)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed('Unknown error')
+    }
   }
 }
 
